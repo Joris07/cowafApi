09350540ca1bb20cff39a7a70f9953f57ad94cd7
Gourdon Joris <jo.070101@gmail.com>
2024-03-09 11:42:24 +0100

    Ajout méthode validation token

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,13 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+#[Route('/api', name: 'login')]
 class SecurityController extends AbstractController
 {
-	#[Route('/api/login', name:"app_login", methods:["POST"])]
+	#[Route('/login', name:"app_login", methods:["POST"])]
 	public function login()
 	{
 		
 	}
+
+	#[Route('/validate', name:"validate_token", methods:["GET"])]
+	public function validateToken()
+    {
+		$user = $this->getUser();
+
+		if ($user instanceof UserInterface) {
+			$userId = $user->getId();
+			return new JsonResponse(['userId' => $userId]);
+		}
+
+		// L'utilisateur n'est pas connecté
+		return new JsonResponse(['valid' => false]);
+    }
 }
